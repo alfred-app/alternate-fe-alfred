08b841406161e97a6b2c5697d2801a29b8ddb690
Derian Melvin <derianmelvin88@gmail.com>
2023-12-08 13:29:30 +0700

    update tailwind config & landing page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,10 @@
-// src/pages/_document.tsx
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
